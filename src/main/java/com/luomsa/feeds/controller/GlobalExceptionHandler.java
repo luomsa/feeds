a17faa32eeef5bfac19da70e58d9e5a75b358017
luomsa <git@luomsa.com>
2024-12-05 16:43:58 +0200

    handle validation problems

--- src/main/java/com/luomsa/feeds/controller/GlobalExceptionHandler.java
+++ src/main/java/com/luomsa/feeds/controller/GlobalExceptionHandler.java
@@ -1,20 +1,43 @@
 package com.luomsa.feeds.controller;
 
 import com.luomsa.feeds.exception.NotFoundException;
+import org.springframework.http.*;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-@ControllerAdvice
+import java.net.URI;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(NotFoundException.class)
     public String handleNotFoundException(NotFoundException e) {
         return e.getMessage();
     }
+
     @ExceptionHandler(UsernameNotFoundException.class)
     public String handleUsernameNotFoundException(UsernameNotFoundException e) {
         return e.getMessage();
     }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        var problem = ProblemDetail.forStatus(HttpStatus.BAD_REQUEST);
+        problem.setTitle("Bad Request");
+        problem.setDetail("Invalid request");
+        problem.setInstance(URI.create(request.getContextPath()));
+        var errors = new HashMap<String, Object>();
+        ex.getBindingResult().getFieldErrors().forEach(error -> {
+            errors.put(error.getField(), error.getDefaultMessage());
+        });
+        problem.setProperties(Map.of("errors", errors));
+        return ResponseEntity.of(problem).build();
+    }
+
 
 }
