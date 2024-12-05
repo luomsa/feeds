36c5091d5dc1ad9698acbc78988a4375802ac3c9
luomsa <git@luomsa.com>
2024-12-05 16:45:50 +0200

    disable creating session without login action

--- src/main/java/com/luomsa/feeds/config/SecurityConfig.java
+++ src/main/java/com/luomsa/feeds/config/SecurityConfig.java
@@ -16,12 +16,14 @@ import org.springframework.security.authentication.dao.DaoAuthenticationProvider
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
 import org.springframework.security.web.context.SecurityContextRepository;
+
 import java.net.URI;
 
 @Configuration
@@ -31,6 +33,7 @@ public class SecurityConfig {
     private final UserRepository userRepository;
     private final Logger logger = LoggerFactory.getLogger(SecurityConfig.class);
     private final ObjectMapper objectMapper = new ObjectMapper();
+
     public SecurityConfig(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
@@ -43,35 +46,44 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .anonymous(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests((authorize) -> authorize
-                        .requestMatchers("/api/auth/login", "/api/auth/register", "/api/feeds/","/error").permitAll()
-                        .requestMatchers(HttpMethod.GET, "/api/posts","/api/posts/{postId}", "/api/posts/{postId}/comments").permitAll()
+                        .requestMatchers("/api/auth/login", "/api/auth/register", "/api/feeds/", "/error", "v3/api-docs").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/posts", "/api/posts/{postId}", "/api/posts/{postId}/comments").permitAll()
                         .anyRequest().authenticated()
                 )
-                .anonymous(AbstractHttpConfigurer::disable)
                 .csrf(AbstractHttpConfigurer::disable)
                 .httpBasic(AbstractHttpConfigurer::disable)
                 .formLogin(AbstractHttpConfigurer::disable)
+                .securityContext(context -> {
+                    context.securityContextRepository(contextRepository());
+                })
+                .requestCache(AbstractHttpConfigurer::disable)
                 .exceptionHandling((exceptionHandling) -> exceptionHandling
                         .authenticationEntryPoint((request, response, authException) -> {
-                            logger.info(String.valueOf(request.isRequestedSessionIdValid()));
-                            logger.info(authException.getClass().getName());
-                            logger.info("Unauthorized");
+                            response.setHeader("Set-Cookie", "JSESSIONID=; Path=/; Max-Age=0; HttpOnly");
                             var problem = ProblemDetail.forStatus(HttpStatus.UNAUTHORIZED);
-                            problem.setTitle("Unauthorized");
-                            problem.setDetail("Unauthorized");
                             problem.setInstance(URI.create(request.getRequestURI()));
                             response.setStatus(problem.getStatus());
                             response.setContentType("application/problem+json");
+                            problem.setTitle("Unauthorized");
+                            if (request.getRequestedSessionId() != null) {
+                                problem.setDetail("Session expired");
+                                objectMapper.writeValue(response.getOutputStream(), problem);
+                                return;
+                            }
+                            problem.setDetail("Unauthorized");
                             objectMapper.writeValue(response.getOutputStream(), problem);
                         })
                 )
                 .sessionManagement(session -> {
                     session.maximumSessions(1);
+                    session.sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED);
                 });
 
         return http.build();
     }
+
     @Bean
     public AuthenticationManager authenticationManager(
             UserDetailsService userDetailsService,
