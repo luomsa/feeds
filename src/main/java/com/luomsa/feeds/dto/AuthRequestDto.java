ca169592be9606b7610d3c556f69c8eac5fd4261
luomsa <git@luomsa.com>
2024-12-06 17:04:06 +0200

    validation tweaks

--- src/main/java/com/luomsa/feeds/dto/AuthRequestDto.java
+++ src/main/java/com/luomsa/feeds/dto/AuthRequestDto.java
@@ -4,6 +4,6 @@ import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
 public record AuthRequestDto(
-        @Size(min = 3, max = 12, message = "Username must be between 3 and 12 characters") @Pattern(regexp = "^[a-zAZ0-9]*$", message = "Username must contain only letters and numbers") String username,
-        @Size(min = 6, max = 12, message = "Password must be between 6 and 12 characters") String password) {
+        @Size(min = 3, max = 14, message = "Username must be between 3 and 14 characters") @Pattern(regexp = "^[a-zAZ0-9]*$", message = "Username must contain only letters and numbers") String username,
+        @Size(min = 8, max = 50, message = "Password must be between 8 and 50 characters") String password) {
 }
--- src/main/java/com/luomsa/feeds/dto/PostRequestDto.java
+++ src/main/java/com/luomsa/feeds/dto/PostRequestDto.java
@@ -3,6 +3,6 @@ package com.luomsa.feeds.dto;
 import jakarta.validation.constraints.Size;
 
 public record PostRequestDto(
-        @Size(min = 1, max = 40, message = "Title must be between 1 and 40 characters") String title,
-        @Size(min = 1, max = 140, message = "Content must be between 1 and 140 characters") String content) {
+        @Size(min = 1, max = 80, message = "Title must be between 1 and 80 characters") String title,
+        @Size(min = 1, max = 400, message = "Content must be between 1 and 400 characters") String content) {
 }
