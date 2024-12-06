15786e5e6fb6d1b8487858aaff579a7da0dab979
luomsa <git@luomsa.com>
2024-12-06 17:00:53 +0200

    add nav bar

--- src/main/frontend/src/routes/App.tsx
+++ src/main/frontend/src/routes/App.tsx
@@ -1,9 +1,11 @@
-import { Container } from "@mantine/core";
 import { Outlet } from "react-router";
+import NavBar from "../components/NavBar.tsx";
+import { Container } from "@mantine/core";
 
 function App() {
   return (
     <Container>
+      <NavBar />
       <Outlet />
     </Container>
   );
